6064f21d9791818355528e7821cc9f34ff0976fd
Daniel Ferrans <danielferrans@hotmail.com>
2015-03-29 21:48:50 -0500

    Testing commit from AS

---
+++ app/src/main/java/com/procesoapp/procesoapp/HttpManager.java
@@ -0,0 +1,32 @@
+package com.procesoapp.procesoapp;
+
+/**
+ * Created by isaac on 25/03/2015.
+ */
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import android.net.http.AndroidHttpClient;
+
+public class HttpManager {
+
+    public static String getData(String uri) {
+
+        AndroidHttpClient client = AndroidHttpClient.newInstance("AndroidAgent");
+        HttpGet request = new HttpGet(uri);
+        HttpResponse response;
+
+        try {
+            response = client.execute(request);
+            return EntityUtils.toString(response.getEntity());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            client.close();
+        }
+
+    }
+
+}
